279b89f4886224591a16fb157e638190025c6073
Jan Bausch <jan.bausch@cobi.bike>
2018-03-07 10:44:45 +0100

    Moved sensible information to env variables

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {};
+
+config.twilio = {};
+// Twilio api credentials from dashbord
+config.twilio.account_sid = process.env.TWILIO_ACCOUNT_SID;
+config.twilio.auth_token = process.env.TWILIO_AUTH_TOKEN;
+// Twilio sender number
+config.twilio.from_number = process.env.TWILIO_FROM_NUMBER;
+
+
+module.exports = config;
